c048999a9a25928b89e244208065a77ec01275d5
Geysa Priscila <geysapriscilasz@gmail.com>
2020-07-05 11:42:39 -0300

    Variáveis Tipos Caractere

---
+++ Caractere.java
@@ -0,0 +1,9 @@
+public class Caractere {
+    public static void main(String args[]) {
+        /*
+         * Variáveis Tipos Caractere char='1'
+         */
+        char letra = 'A';
+        System.out.println(letra);
+    }
+}
