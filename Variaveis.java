dbc41036d83c53fa0314f13a61b8c2a02228e567
Geysa Priscila <geysapriscilasz@gmail.com>
2020-07-05 11:31:21 -0300

    Variáveis Tipos Numéricos Inteiros

---
+++ Variaveis.java
@@ -0,0 +1,9 @@
+public class Variaveis {
+    public static void main(String args[]){
+        /* Variáveis Tipos Numéricos Inteiros
+        byte=127, short=42, int=3, long=22L */
+        int ano;
+        ano = 2020;
+        System.out.println(ano);
+    }
+}
