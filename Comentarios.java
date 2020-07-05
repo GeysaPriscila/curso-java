0cd3119b04466358831ad0afb13d804daabcdd3e
Geysa Priscila <geysapriscilasz@gmail.com>
2020-07-05 10:46:27 -0300

    Aula de comentários.

---
+++ Comentarios.java
@@ -0,0 +1,8 @@
+// Classe principal
+public class Comentarios {
+    // Método principal
+    public static void main(String args[]) {
+        /* Imprime a mensagem na tela */
+        System.out.println("Aula de Comentarios.");
+    }
+}
