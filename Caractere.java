cd549aec38f83ea662139e3e9987a462a60eff75
Geysa Priscila <geysapriscilasz@gmail.com>
2020-07-05 12:21:53 -0300

    Tipos de variáveis

--- Caractere.java
+++
@@ -1,9 +0,0 @@
-public class Caractere {
-    public static void main(String args[]) {
-        /*
-         * Variáveis Tipos Caractere char='1'
-         */
-        char letra = 'A';
-        System.out.println(letra);
-    }
-}
--- VariaveisInteiros.java
+++
@@ -1,9 +0,0 @@
-public class VariaveisInteiros {
-    public static void main(String args[]){
-        /* Variáveis Tipos Numéricos Inteiros
-        byte=127, short=42, int=3, long=22L */
-        int ano;
-        ano = 2020;
-        System.out.println(ano);
-    }
-}
