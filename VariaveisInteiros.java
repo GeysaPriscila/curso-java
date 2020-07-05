474ed982f2ad5a5001db470247272aad99b599e7
Geysa Priscila <geysapriscilasz@gmail.com>
2020-07-05 11:33:34 -0300

    Ajustes

--- VariaveisInteiros.java
+++ VariaveisInteiros.java
@@ -1,4 +1,4 @@
-public class Variaveis {
+public class VariaveisInteiros {
     public static void main(String args[]){
         /* Variáveis Tipos Numéricos Inteiros
         byte=127, short=42, int=3, long=22L */
