a34eee0a8321d7e8deab8ec38b2ec55c13615107
Geysa Priscila <geysapriscilasz@gmail.com>
2020-07-05 15:10:03 -0300

    Operadores aritméticos

---
+++ Operadores.java
@@ -0,0 +1,22 @@
+/**
+ * Operadores aritméticos
+ */
+public class Operadores {
+    public static void main(String args[]) {
+        int n1 = 10, n2 = 2;
+
+        int soma = n1 + n2;
+        int subtracao = n1 - n2;
+        int multiplicacao = n1 * n2;
+        int divisao = n1 / n2;
+        int resto = n1 % n2;
+
+        // int resultado = divisao;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtracao: " + subtracao);
+        System.out.println("Multiplicacao: " + multiplicacao);
+        System.out.println("Divisao: " + divisao);
+        System.out.println("Resto da divisao: " + resto);
+    }
+}
