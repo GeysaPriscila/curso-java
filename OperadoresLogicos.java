75af3eb4003bd66a2a443bfb9cdfdee5604481a4
Geysa Priscila <geysapriscilasz@gmail.com>
2020-07-19 12:31:21 -0300

    Operadores Logicos

--- OperadoresLogicos.java
+++ OperadoresLogicos.java
@@ -5,8 +5,8 @@ public class OperadoresLogicos {
     public static void main(String[] args) {
         int a = 2, b = 3;
 
-        boolean result1 = a > b && a == b;
-        boolean result2 = a < b || a == b;
+        boolean result1 = (a > b) && (a != b);
+        boolean result2 = (a < b) || (a == b);
 
         System.out.println(result1);
         System.out.println(result2);
