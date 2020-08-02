5d71e3b8e90dfb2fea63bd0fe523caaec29cbc7b
Geysa Priscila <geysapriscilasz@gmail.com>
2020-08-02 13:17:24 -0300

    Operador Ternario

---
+++ OperadorTernario.java
@@ -0,0 +1,7 @@
+public class OperadorTernario {
+    public static void main(String args[]) {
+        int dia = 10;
+
+        System.out.println((dia <= 15) ? ("Primeira quinzena") : ("Segunda quinzena"));
+    }
+}
