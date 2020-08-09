82052744be6f68fc3defd43ea2147c8f70b734fa
Geysa Priscila <geysapriscilasz@gmail.com>
2020-08-09 17:54:48 -0300

    Estrutura de Repetição 1

---
+++ Loop.java
@@ -0,0 +1,12 @@
+public class Loop{
+    public static void main(String args[]) {
+        int num = 1;
+
+        while(num < 11){
+            System.out.println(num);
+            num++;
+        }
+
+        System.out.println("Fora do Loop");
+    }
+}
